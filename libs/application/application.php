e407fb6f25a40ae3ec864f9c8789a25f38653c32
emirhanengn <almostoverr@gmail.com>
2017-05-14 00:48:30 +0200

    BetaV2

--- libs/application/application.php
+++ libs/application/application.php
@@ -5,13 +5,12 @@ class Application
 {
   public static function set()
   {
+    
   }
   public static function get()
   {
-      echo 'Çalıştı!';
-      echo 'Nayır çalışmadı!';
+
   }
-  
   public static function run()
   {
 
