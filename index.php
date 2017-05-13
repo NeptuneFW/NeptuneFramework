0aedb341fa01d8b12209b270a30e0b59f957b205
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-13 22:00:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,4 @@ require_once 'init.php';
 use System\Application;
 
 Application::set('development', '/development', 'ip', ['127.0.0.1', '::1']);
-Application::set('admin', '/admin', 'public', '');
 Application::end();
