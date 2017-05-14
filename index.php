dcba99ee671ebdef9cbae07464aa1b39a69542af
emirhanengn <almostoverr@gmail.com>
2017-05-14 18:01:53 +0200

    BetaV2

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
 require_once realpath('.') . '\system\init.php';
-
-use Libs\Router\Router;
-
-$app = new Router();
-
-$app->get('/', 'HomeController.index');
-
-$app->run();
--- system/init.php
+++ system/init.php
@@ -4,3 +4,11 @@ define('ROOT', realpath('.') . DS);
 define('DEFAULT_APP', 'production');
 
 require_once ROOT . 'vendor\autoload.php';
+
+use Libs\Router\Router;
+
+$app = new Router();
+
+$app->get('/', 'HomeController.index');
+
+$app->run();
